a17be6ab9d570aef0f421640d5f4fe391c5b767d
Angela Venable <avenable11@ivytech.edu>
2026-06-08 16:02:13 -0400

    car class

--- 06_08_26/main.cpp
+++ 06_08_26/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+
+class car
+{
+
+    std::string make;
+    std::string model;
+    int year;
+    long mileage;
+    std::string color;
+    double GVWR; // gross vehicle weight rating
+    bool started;
+
+public:
+    void startCar();
+};
 
 int main()
 {
+    car c;
+    c.make = "honda";
     std::cout << "hello world" << std::endl;
     return 0;
 }
