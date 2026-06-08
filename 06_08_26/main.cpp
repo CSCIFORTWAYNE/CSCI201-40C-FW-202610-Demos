7a59f039188095e2186910aac4282ede93a23e67
Angela Venable <avenable11@ivytech.edu>
2026-06-08 16:49:02 -0400

    start of clocktype

--- 06_08_26/main.cpp
+++ 06_08_26/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
-class car
+class Car
 {
 
     std::string make;
@@ -16,10 +17,49 @@ public:
     void startCar();
 };
 
+class ClockType
+{
+
+public:
+    void setTime(int hour, int minute, int second);
+    void getTime(int &hour, int &minute, int &second) const;
+    void printTime() const;
+    void incrementSeconds();
+    void incrementMinutes();
+    void incrementHours();
+    bool equalTime(const ClockType &otherClock) const;
+
+private:
+    int hr;
+    int min;
+    int sec;
+};
+
 int main()
 {
-    car c;
+    Car c;
     // c.make = "honda";
     std::cout << "hello world" << std::endl;
     return 0;
+}
+
+void ClockType::setTime(int hour, int minute, int second)
+{
+    if (hour < 0)
+    {
+        hour = abs(hour);
+    }
+
+    if (hour >= 0 && hour <= 23)
+    {
+        hr = hour;
+    }
+    else
+    {
+        hr = hour % 24;
+    }
+    // can replace all if statements with hr = abs(hour) % 24;
+
+    min = abs(minute) % 60;
+    sec = abs(second) % 60;
 }
