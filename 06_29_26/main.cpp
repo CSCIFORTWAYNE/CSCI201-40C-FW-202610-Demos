faac396b166d9924e6b5693dde7a556fc43c3bf9
Angela Venable <avenable11@ivytech.edu>
2026-06-29 14:58:59 -0400

    adding pointer parameter

--- 06_29_26/main.cpp
+++ 06_29_26/main.cpp
@@ -2,17 +2,17 @@
 #include "person.h"
 #include "employee.h"
 
-void printPerson(Person &p);
+void printPerson(Person *p);
 
 int main()
 {
     Employee emp("Harry Potter", 13, "555-1212", "hpotter@email.com", 23000.21, "2021-12-12");
     Person per("Charlie Brown", 11, "555-1234", "cbrown@email.com");
-    printPerson(emp);
+    printPerson(&emp);
     return 0;
 }
 
-void printPerson(Person &p)
+void printPerson(Person *p)
 {
-    std::cout << p.toString() << std::endl;
+    std::cout << p->toString() << std::endl;
 }
